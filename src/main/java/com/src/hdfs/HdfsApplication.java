5b1fdedbc6de24bb820204bf7565922f4421b053
real-syjo <csy815kr@gmail.com>
2023-02-10 20:50:06 +0900

    modified config

--- src/main/java/com/src/hdfs/HdfsApplication.java
+++ src/main/java/com/src/hdfs/HdfsApplication.java
@@ -2,10 +2,17 @@ package com.src.hdfs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HdfsApplication {
+public class HdfsApplication extends SpringBootServletInitializer{
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) { 
+		return application.sources(HdfsApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HdfsApplication.class, args);
 	}
